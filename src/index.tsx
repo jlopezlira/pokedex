62bbe7f0cac81461c8d00e0ddb7a59dcdcd7f342
Juan Jóse López Lira <jlopezlira@gmail.com>
2021-01-11 23:13:00 -0500

    PWA

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import './assets/styles/GlobalStyles.css'
 
+import * as serviceWorker from './serviceWorker'
+
 import App from './App'
 import React from 'react'
 import ReactDOM from 'react-dom'
@@ -16,3 +18,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
+serviceWorker.register()
